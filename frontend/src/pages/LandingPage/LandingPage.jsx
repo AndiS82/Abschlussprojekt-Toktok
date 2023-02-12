ce33c1a1fd6e1e62c958ceedf0a142be6970ce6b
sofia28304 <sofia.vogelhaupt@yahoo.de>
2023-02-12 14:02:19 +0100

    landingpage test

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,9 +1,22 @@
 import './LandingPage.css'
+import { useNavigate } from 'react-router-dom'
+import LoginPages from './pages/LoginPages/LoginPages';
+import LandLogo from '../../img/Group.png'
 
-const LandingPage = () => {
+function LandingPage() {
+    const redirect = useNavigate()
+    useEffect(() => {
+        setTimeout(() => {
+            redirect('/Login')
+        }, 3000)
+    }
+
+    )
+
     return (
         <div>
-
+            <img className="LandLogo" src={LandLogo} />
+            <p>Hier ist dann die Warteanimation</p>
         </div>
     );
 }
