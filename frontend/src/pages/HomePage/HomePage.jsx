f231b7537bcffe2fcdf5db7df858a4363503b3dc
jAs0n1337 <steffen.dietrich86@gmail.com>
2023-02-13 10:38:15 +0100

    Miniturprofil added

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -2,16 +2,24 @@ import './HomePage.css'
 import { Link } from "react-router-dom";
 import { FaRegHeart } from "react-icons/fa";
 import MiniLogo from '../../img/LogoMini.png';
+import ProfilMini from '../../components/ProfilMini/ProfilMini';
 
-const HomePage = ({ user }) => {
+const HomePage = () => {
     return (
-        <nav className="navbar">
-            <img src={MiniLogo} alt="Your Image" />
-            <h1>Toktok</h1>
-            <Link to="/UnderConstruction">
-                <FaRegHeart className="icon" />
-            </Link>
-        </nav>
+        <>
+            <div>
+                <nav className="homeNavbar">
+                    <img src={MiniLogo} alt="MiniLogo" />
+                    <Link to="/Home">
+                        <h1>Toktok</h1>
+                    </Link>
+                    <Link to="/UnderConstruction">
+                        <FaRegHeart className="homeIcon" />
+                    </Link>
+                </nav>
+            </div>
+            <ProfilMini />
+        </>
     );
 };
 
