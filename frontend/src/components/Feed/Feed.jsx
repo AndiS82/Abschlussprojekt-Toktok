eaf358f09fd6d8c6f2e8ded56883b342d12a6aa7
emilysupercode <sheil.emily.sc@gmail.com>
2023-02-18 19:51:01 +0100

    feed wartet auf userLoaded

--- frontend/src/components/Feed/Feed.jsx
+++ frontend/src/components/Feed/Feed.jsx
@@ -1,9 +1,31 @@
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Profil from '../Profil/Profil'
 import './Feed.css'
 
-const Feed = ({ rerender, setRerender }) => {
+const Feed = ({ userLoaded }) => {
     const [feed, setFeed] = useState([])
+    // const nav = useNavigate()
+    // useEffect(() => {
+    //     const getUser = async () => {
+    //         setUserLoaded(false)
+    //         const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/user`,
+    //             {
+    //                 credentials: 'include'
+    //             })
+    //         if (response.ok) {
+    //             const data = await response.json()
+    //             setUserData(data)
+    //             setUserLoaded(true)
+    //             // console.log(data)
+    //         }
+    //         else {
+    //             nav('/')
+    //         }
+    //     }
+    //     getUser()
+    // }, [])
+
     useEffect(() => {
         const getFeed = async () => {
             const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/posts`,
@@ -13,6 +35,7 @@ const Feed = ({ rerender, setRerender }) => {
             if (response.ok) {
                 const data = await response.json()
                 setFeed(data)
+                console.log(data)
             }
             else {
                 console.log('feed not loaded')
@@ -23,9 +46,14 @@ const Feed = ({ rerender, setRerender }) => {
 
     return (
         <div>
-            {feed?.map((singlePost, key) => {
-                return <Profil key={key} singlePost={singlePost} rerender={rerender} setRerender={setRerender} />
-            })}
+            {userLoaded &&
+                <>
+                    {feed?.map((singlePost, key) => {
+                        return <Profil key={key} singlePost={singlePost} />
+                    })}
+                </>
+            }
+            {!userLoaded && <p>Loading...</p>}
         </div>
     );
 }
